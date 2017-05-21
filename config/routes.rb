25b5ba5f259e2bb1b51c1c43fc9704a34ac3a29a
Trai Compton <tlcompton2546@outlook.com>
2017-05-21 14:23:59 +0000

    setting up home page and navigation links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/home'
+  # Root Path
+  root 'welcome#home'
 
+  # Devise
+  devise_for :users
+
+  # Resources 
+
+
+  # Regular Routes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
