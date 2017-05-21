bdc069c83c60f27ec42b0dddd1adb03b015d214a
Trai Compton <tlcompton2546@outlook.com>
2017-05-21 14:58:19 +0000

    Setting up BooksController and Index Template

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,32 @@
 class BooksController < ApplicationController
-  belongs_to :user
+  before_action :authenticate_user!
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
 
-  has_many :chapters
-  has_many :pages, through: :chapters
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
 end
