5fe79107fae3dc3c7d410f0ba02319fa81a7bf99
Trai Compton <tlcompton2546@outlook.com>
2017-05-21 18:35:32 +0000

    Finishing up EntriesController

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,16 +2,20 @@ class EntriesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_entry, only: [:show, :edit, :update, :destroy]
 
-  def index
-  end
-
-  def show
-  end
-
   def new
+    @entry = Entry.new(page_id: params[:page_id])
   end
 
   def create
+    @entry = Entry.new(entry_params)
+
+    if @entry.valid?
+      @entry.save
+
+      redirect_to page_path(@entry.page)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -27,5 +31,9 @@ class EntriesController < ApplicationController
 
   def set_entry
     @entry = Entry.find(params[:id])
+  end
+
+  def entry_params
+    params.require(:entry).permit(:mood, :content, :page_id)
   end
 end
